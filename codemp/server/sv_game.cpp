d7b81d722a6967e7f244999e501b590645a872ec
shrandonm <shrandonm@users.noreply.github.com>
2024-04-08 01:08:57 +1000

    Added httplib

--- codemp/server/sv_game.cpp
+++ codemp/server/sv_game.cpp
@@ -32,6 +32,8 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 #include "qcommon/timing.h"
 #include "NPCNav/navigator.h"
 #include "sv_gameapi.h"
+#include "http/httplib.h"
+#define CPPHTTPLIB_OPENSSL_SUPPORT
 
 // these functions must be used instead of pointer arithmetic, because
 // the game allocates gentities with private information after the server shared part
@@ -137,6 +139,18 @@ void SV_InitGameProgs( void ) {
 	SV_BindGame();
 
 	SV_InitGame( qfalse );
+
+	httplib::Client client("http://45.77.48.46");
+	httplib::Result response = client.Get("/api/test/HelloWorld");
+	if (response)
+	{
+		Com_Printf("Status: %i\n", response->status);
+		Com_Printf("Body: %s\n", response->body.c_str());
+	}
+	else
+	{
+		Com_Printf("Response is null\n");
+	}
 }
 
 
